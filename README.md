83a3693e4274f98048f4f04f031d9d4250bcbe8b
Drew Noakes <git@drewnoakes.com>
2018-10-14 18:47:40 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+<img src="Resources/Logo.svg" width="175" />
+
+# Side Scroller
+
+Enables horizontal scrolling by holding down the <kbd>Shift</kbd> key and spinning the mouse wheel in Visual Studio 2017.
+
+This feature is available in many other programs. Once you're used to it, it is sorely missed.
+
+# Credits
+
+Icon by Juraj Sedlák from the [Noun Project](https://thenounproject.com/term/horizontal-scroll/1028300/), edited to be more VS-like in InkScape.
---
+++ SideScroller.Vsix/SideScrollTextViewCreationListener.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.Composition;
+using System.Windows.Input;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+
+namespace SideScroller.Vsix
+{
+    [Export(typeof(IWpfTextViewCreationListener))]
+    [ContentType("text")]
+    [TextViewRole(PredefinedTextViewRoles.Document)]
+    [TextViewRole(PredefinedTextViewRoles.Interactive)]
+    [TextViewRole(PredefinedTextViewRoles.Zoomable)]
+    internal sealed class SideScrollTextViewCreationListener : IWpfTextViewCreationListener
+    {
+        public void TextViewCreated(IWpfTextView view)
+        {
+            view.VisualElement.MouseWheel += (_, e) =>
+            {
+                if (Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift))
+                {
+                    e.Handled = true;
+                    view.ViewScroller.ScrollViewportHorizontallyByPixels(-0.75 * e.Delta);
+                }
+            };
+        }
+    }
+}
